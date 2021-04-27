aa85940877fcd55c82dd670870abe75e52ca3d2a
demetriomagatti <demetriomagatti@gmail.com>
2021-04-27 16:13:43 +0200

    MNIST_extra tests completed - working ModelBuilder class

--- Lesson3/local/builder.py
+++ Lesson3/local/builder.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import preprocessing as pr
+import math
+from local import preprocessing as pr
 
 
 class ModelBuilder:
@@ -8,6 +9,8 @@ class ModelBuilder:
         # Default parameters
         self.dataset = None
         self.metadata = None
+        self.model = None
+        self.history = None
         self.flatten_input = True
         self.normalize = True
         self.cache_dataset  = True
@@ -28,7 +31,7 @@ class ModelBuilder:
 
 
     def define(self):
-        model = keras.Sequential()
+        model = tf.keras.Sequential()
         if self.flatten_input:
             model.add(tf.keras.layers.Flatten(input_shape=self.input_shape))
         if isinstance(self.hidden_layer_neurons, int):
@@ -41,14 +44,26 @@ class ModelBuilder:
         return model
 
 
-    def train(self,model):
-        train_dataset, test_dataset = self.dataset['train'], self.dataset['test']
-        class_names = self.metadata.features['label'].names
+    def train(self):
+        train_dataset = self.dataset['train']
         num_train_examples = self.metadata.splits['train'].num_examples
-        num_test_examples = self.metadata.splits['test'].num_examples
         if self.normalize:
-            train_dataset.map(pr.normalize)
+            train_dataset = train_dataset.map(pr.normalize)
         if self.cache_dataset:
             train_dataset =  train_dataset.cache()
-        model.compile(optimizer=self.optimizer,loss=self.loss,metrics=self.metrics)
-        return train_history = model.fit(train_dataset, epochs=self.epochs, steps_per_epoch=math.ceil(num_train_examples/BATCH_SIZE))
+        if self.shuffle_train:
+            train_dataset = train_dataset.repeat().shuffle(num_train_examples)
+        train_dataset = train_dataset.batch(self.batch_size)
+        self.model.compile(optimizer=self.optimizer,loss=self.loss,metrics=self.metrics)
+        return self.model.fit(train_dataset, epochs=self.epochs, steps_per_epoch=math.ceil(num_train_examples/self.batch_size))
+
+
+    def evaluate(self):
+        test_dataset = self.dataset['test']
+        num_test_examples = self.metadata.splits['test'].num_examples
+        if self.normalize:
+            test_dataset = test_dataset.map(pr.normalize)
+        if self.cache_dataset:
+            test_dataset =  test_dataset.cache()
+        test_dataset = test_dataset.batch(self.batch_size)
+        return self.model.evaluate(test_dataset, steps=math.ceil(num_test_examples/self.batch_size))
